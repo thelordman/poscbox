bb89dd1071212cbb9aeefa0e1f036ccabc94b7c6
hotpocket <ml0938622@gmail.com>
2023-06-19 08:20:47 -0400

    No combat in mines

--- src/main/java/me/lord/poscbox/listeners/PlayerDamageListener.java
+++ src/main/java/me/lord/poscbox/listeners/PlayerDamageListener.java
@@ -11,6 +11,10 @@ public class PlayerDamageListener implements Listener {
     @EventHandler
     public void onDamage(EntityDamageByEntityEvent event) {
         if (event.getDamager() instanceof Player attacker && event.getEntity() instanceof Player victim) {
+            if (event.getEntity().getLocation().getY() < -25 && !CombatLog.inCombat(victim)) {
+                event.setCancelled(true);
+                return;
+            }
             CombatLog.setCombat(attacker);
             CombatLog.setCombat(victim);
         }
